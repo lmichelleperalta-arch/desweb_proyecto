904d7ae78fd8e73d027704aa32858b3e353ea3ec
Michelle <lmichelleperalta@gmail.com>
2026-04-24 15:18:54 +0200

    Modelos postman

--- djangoapi/catastrodj/urls.py
+++ djangoapi/catastrodj/urls.py
@@ -10,5 +10,16 @@ urlpatterns = [
     path("prediosdelete2_catastrodj/<int:id>/",views.PrediosDelete2.as_view(),name="prediosdelete2_catastrodj"),
     path("prediosselectall_catastrodj/",views.PrediosSelectAll.as_view(),name="prediosselectall_catastrodj"),
     path("prediosselectasdicts_catastrodj/<int:id>/",views.PrediosSelectAsDicts.as_view(),name="prediosselectasdicts_catastrodj"),
-
+    path("prediosupdate_catastrodj/<int:id>/",views.PrediosUpdate.as_view(),name="prediosupdate_catastrodj"),
+    path("callesinsert_catastrodj/", views.CallesInsert.as_view(),name="callesinsert_catastrodj"),
+    path("callesdelete_catastrodj/<int:id>/",views.CallesDelete.as_view(),name="callesdelete_catastrodj"),
+    path("callesselectall_catastrodj/",views.CallesSelectAll.as_view(),name="callesselectall_catastrodj"),
+    path("callesselectasdicts_catastrodj/<int:id>/",views.CallesSelectAsDicts.as_view(),name="callesselectasdicts_catastrodj"),
+    path("callesupdate_catastrodj/<int:id>/",views.CallesUpdate.as_view(),name="callesupdate_catastrodj_catastrodj"),
+    path("iglesiasinsert_catastrodj/", views.IglesiasInsert.as_view(),name="iglesiasinsert_catastrodj"),
+    path("iglesiasdelete_catastrodj/<int:id>/",views. IglesiasDelete.as_view(),name="iglesiasdelete_catastrodj"),
+    path("iglesiasselectall_catastrodj/",views.IglesiasSelectAll.as_view(),name="iglesiasselectall_catastrodj"),
+    path("iglesiasselectasdicts_catastrodj/<int:id>/",views.IglesiasSelectAsDicts.as_view(),name="iglesiasselectasdicts_catastrodj"),
+    path("iglesiasupdate_catastrodj/<int:id>/",views.IglesiasUpdate.as_view(),name="iglesiasupdate_catastrodj_catastrodj_catastrodj"),
+   
 ]
--- djangoapi/catastrodj/views.py
+++ djangoapi/catastrodj/views.py
@@ -9,7 +9,9 @@ from core.myLib.geometryTools import WkbConversor, GeometryChecks
 
 from catastrodj import models
 from catastrodj.operations.prediodj import insert,delete, selectallAsDicts, update, selectAsDicts
-from catastrodj.operations.callesdj import insert as insert_calles ,delete, selectallAsDicts, update, selectAsDicts
+from catastrodj.operations.callesdj import insert as insert_c ,delete as delete_c, selectallAsDicts as selectallAsDicts_c, update as update_c, selectAsDicts as selectAsDicts_c
+from catastrodj.operations.iglesiasdj import insert as insert_i ,delete as delete_i, selectallAsDicts as selectallAsDicts_i, update as update_i, selectAsDicts as selectAsDicts_i
+
 
 class HelloCatastrodj(View):
     def get(self, request):
@@ -17,6 +19,7 @@ class HelloCatastrodj(View):
     def post(self, request):
         return JsonResponse({"ok":True,"message": "HelloCatastrodj. Hello world", "data":[request.POST.dict()]})
 
+# Clase Predios. 
 class PrediosInsert(View):
     def post(self, request):
         d=request.POST.dict()
@@ -50,7 +53,70 @@ class PrediosUpdate(View):
 
 class PrediosSelectAsDicts(View):
     def get(self, request, id):
-        d=request.GET.dict()
         d = {"id": id}
         r=selectAsDicts(d)
         return JsonResponse(r)
+    
+#Clases Calles
+class CallesInsert(View):
+    def post(self, request):
+        d=request.POST.dict()
+        r=insert_c(d)
+        return JsonResponse(r)
+
+class CallesDelete(View):
+    def post(self, request, id):
+        d = {"id": id}
+        r = delete_c(d)
+        return JsonResponse(r)
+
+#GET por que solo estamos seleccionando
+class CallesSelectAll(View):
+    def get(self, request):
+        r = selectallAsDicts_c()
+        return JsonResponse(r)
+    
+class CallesUpdate(View):
+    def post(self, request):
+        d=request.POST.dict()
+        r=update_c(d)
+        return JsonResponse(r)
+
+class CallesSelectAsDicts(View):
+    def get(self, request, id):
+        d = {"id": id}
+        r=selectAsDicts_c(d)
+        return JsonResponse(r)
+    
+#Clases Iglesias
+class IglesiasInsert(View):
+    def post(self, request):
+        d=request.POST.dict()
+        r=insert_i(d)
+        return JsonResponse(r)
+
+class IglesiasDelete(View):
+    def post(self, request, id):
+        d = {"id": id}
+        r = delete_i(d)
+        return JsonResponse(r)
+
+#GET por que solo estamos seleccionando
+class IglesiasSelectAll(View):
+    def get(self, request):
+        r = selectallAsDicts_i()
+        return JsonResponse(r)
+    
+class IglesiasUpdate(View):
+    def post(self, request):
+        d=request.POST.dict()
+        r=update_i(d)
+        return JsonResponse(r)
+
+class IglesiasSelectAsDicts(View):
+    def get(self, request, id):
+        d=request.GET.dict()
+        d = {"id": id}
+        r=selectAsDicts_i(d)
+        return JsonResponse(r)
+    
