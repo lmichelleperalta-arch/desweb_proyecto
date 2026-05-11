0d3ba3ae0a3809a9971b498aa947cefbb21c650f
Michelle <lmichelleperalta@gmail.com>
2026-05-11 13:27:10 +0200

    VersionPostman_chat

--- djangoapi/catastrodj/views.py
+++ djangoapi/catastrodj/views.py
@@ -3,6 +3,12 @@
 from django.http import JsonResponse
 from django.views import View
 
+## A ver si funciona
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
+@method_decorator(csrf_exempt, name='dispatch')
+##Si no borramos
 
 #My imports
 from core.myLib.geometryTools import WkbConversor, GeometryChecks
